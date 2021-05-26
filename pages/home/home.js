126bb84e6f5681efc558bcbca1ffa6d2cd57da80
justchoe <1620845677@qq.com>
2021-05-26 11:06:42 +0800

    完成

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,18 +1,81 @@
+const {http} = require('../../lib/http.js');
 
 Page({
+  updateId: '',
+  updatIndex: '',
   data: {
-    lists: [
-      { id: 1,text: "事实上是事实上事实上是事实上",selected: true},{ id: 2,text: "事实上是事实上事实上是事实上",selected: true},{ id: 3,text: "事实上是事实上事实上是事实上",selected: true},{ id: 4,text: "事实上是事实上事实上是事实上",selected: true},{ id: 5,text: "事实上是事实上事实上是事实上",selected: true},{ id: 6,text: "事实上是事实上事实上是事实上",selected: true},{ id: 7,text: "事实上是事实上事实上是事实上",selected: true}
-    ],
-    visibleConfirm: false
+    lists: [],
+    visibleCreateConfirm: false,
+    visibleUpdateConfirm: false,
+    updateContent: "",
+    hidden: false
   },
-  confirm(event){
-
+  onShow(){
+    http.get('/todos?completed=false').then(res=>{
+      this.setData({ lists: res.data.resources })
+    })
   },
-  calcel(event){
-
+  confirmCreate(e){
+    let content = e.detail
+    if (content) {
+      http.post('/todos',{
+          completed: false, description: content
+      })
+      .then(res => {
+        let todo = [res.data.resource]
+        this.data.lists = todo.concat(this.data.lists)
+        this.setData({ lists: this.data.lists })
+        this.hideCreateConfirm()
+      })
+    }
   },
-  destoryItem(){
+  changeText(e){
+    let {content,id,index} = e.currentTarget.dataset
+    this.updateId = id
+    this.updatIndex = index
+    this.setData({ visibleUpdateConfirm: true, updateContent: content})
+  },
+  confirmUpdate(e){
+    let content = e.detail
+    http.put(`/todos/${this.updateId}`, {
+      description: content
+    })
+    .then(res => {
+      let todo = res.data.resource
+      this.data.lists[this.updatIndex] = todo
+      this.setData({ lists: this.data.lists })
+      this.hideUpdateConfirm()
+    })
+  },
+  destroyTodo(e){
+    let index = e.currentTarget.dataset.index
+    let id = e.currentTarget.dataset.id
+    http.put(`/todos/${id}`,{
+      completed: true
+    })
+    .then(res => {
+      //销毁list
+      let todo = res.data.resource
+      this.data.lists[index] = todo
+      this.setData({ lists: this.data.lists })
+      wx.showToast({
+        title: '完成待办',
+        icon: 'success',
+        duration: 2000
+      })
 
+    })
+  },
+  vibrate(){
+    wx.vibrateLong()
+  },
+  hideCreateConfirm(){
+    this.setData({ visibleCreateConfirm: false })
+  },
+  showCreateConfirm(){
+    this.setData({ visibleCreateConfirm: true })
+  },
+  hideUpdateConfirm(){
+    this.setData({ visibleUpdateConfirm: false })
   }
 })
--- pages/tomato/tomato.js
+++ pages/tomato/tomato.js
@@ -1,33 +1,38 @@
-// pages/tomato/tomato.js
+const { http } = require('../../lib/http.js');
+
 Page({
   timer: null,
   data: {
-    defaultSecond: 1500,
+    defalutSecond: 1500,
     time: "",
     timerStatus: 'stop',
     confirmVisible: false,
     againButtonVisible: false,
-    finishConfirmVisible: false
+    finishConfirmVisible: false,
+    tomato: {}
   },
   onShow: function () {
     this.startTimer()
+    http.post('/tomatoes').then(res => {
+      this.setData({ tomato: res.data.resource })
+    })
   },
   startTimer(){
-    this.setData({timerStatus:'start'})
+    this.setData({ timerStatus: 'start' })
     this.changeTime()
-    this.timer = setInterval(()=>{
-      this.data.defaultSecond = this.data.defaultSecond - 1
+    this.timer = setInterval(() => {
+      this.data.defalutSecond = this.data.defalutSecond - 1
       this.changeTime()
-      if(this.data.defaultSecond <= 0){
-        this.setData({ againButtonVisible: true})
-        this.setData({finishConfirmVisible : true})
-         return this.clearTimer()
+      if (this.data.defalutSecond <= 0){
+        this.setData({ againButtonVisible: true })
+        this.setData({ finishConfirmVisible: true })
+        return this.clearTimer()
       }
-    },1000)
+    }, 1000)
   },
   againTimer(){
-    this.data.defaultSecond = 1500
-    this.setData({ againButtonVisible: false})
+    this.data.defalutSecond = 1500
+    this.setData({ againButtonVisible: false })
     this.startTimer()
   },
   clearTimer(){
@@ -35,39 +40,63 @@ Page({
     this.timer = null
     this.setData({ timerStatus: 'stop' })
   },
-changeTime(){
-  let m = Math.floor(this.data.defaultSecond/60)
-  let s = Math.floor(this.data.defaultSecond%60)
-  if(s===0){
-    s= "00"
-  }
-  if((s+"").length === 1){
-   s="0"+ s
-  }
-  if((m+"").length === 1){
-    m="0"+ m
-   }
-  this.setData({ time: `${m}:${s}` })
-},
-confirmAbandon(event){
- let content = event.detail
- wx.navigateBack({
-   to: -1
- })
-},
-confirmFinish(event){
-let content = event.detail
-},
-confirmCancel(){
-this.setData({finishConfirmVisible: false})
-},
-showConfirm(){
-this.setData({confirmVisible: true})
-this.clearTimer()
-},
-hideConfirm(){
-  this.setData({confirmVisible: false})
-  this.startTimer()
-}
-
+  changeTime(){
+    let m = Math.floor(this.data.defalutSecond/60)
+    let s = Math.floor(this.data.defalutSecond%60)
+    if(s === 0){
+      s = "00"
+    }
+    if((s+"").length === 1){
+      s = "0" + s
+    }
+    if ((m + "").length === 1) {
+      m = "0" + m
+    }
+    this.setData({ time: `${m}:${s}` })
+  },
+  confirmAbandon(e){
+    let content = e.detail
+    http.put(`/tomatoes/${this.data.tomato.id}`,{
+      description: content,
+      aborted: true
+    })
+    .then(res => {
+      wx.navigateBack({ to: -1 })
+    })
+  },
+  confirmFinish(e){
+    let content = e.detail
+    http.put(`/tomatoes/${this.data.tomato.id}`,{
+      description: content,
+      aborted: true
+    })
+      .then(res => {
+        wx.navigateBack({ to: -1 })
+      })
+  },
+  confirmCancel(){
+    this.setData({ finishConfirmVisible: false })
+  },
+  showConfirm(){
+    this.setData({ confirmVisible: true })
+    this.clearTimer()
+  },
+  hideConfirm(){
+    this.setData({ confirmVisible: false })
+    this.startTimer()
+  },
+  onHide() {
+    this.clearTimer()
+    http.put(`/tomatoes/${this.data.tomato.id}`, {
+      description: "退出放弃",
+      aborted: true
+    })
+  },
+  // onUnload() {
+  //   this.clearTimer()
+  //   http.put(`/tomatoes/${this.data.tomato.id}`, {
+  //     description: "退出放弃",
+  //     aborted: true
+  //   })
+  // },
 })
